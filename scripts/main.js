33d2498feb931cc917c319c7cb72be7c7b59e552
nwshane <django09@gmail.com>
2016-04-19 11:18:45 +0400

    Replaced jQuery code with vanilla JavaScript in order to remove dependency on jQuery.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-  $('.js-toggle-navigation').click(function() {
-    $('.js-act-as-navigation').toggleClass('is-hidden-on-mobile');
-  });
-});
+// Vanilla JavaScript solution based on this article:
+// https://www.jamestease.co.uk/blether/add-remove-or-toggle-classes-using-vanilla-javascript
+
+var gogoCode = (function() {
+  gogoCode = {};
+
+  gogoCode.setupMobileNavButton = function() {
+    document.querySelector('.js-toggle-navigation').addEventListener('click', function(e) {
+      e.preventDefault();
+
+      [].map.call(document.querySelectorAll('.js-act-as-navigation'), function(el) {
+        el.classList.toggle('is-hidden-on-mobile');
+      });
+    });
+  }
+
+  return gogoCode;
+})();
+
+gogoCode.setupMobileNavButton();
